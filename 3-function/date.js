e67e888010a38e6dfc3be0cdb9dc9596b7c56218
emadsorush <emadsorush@gmail.com>
2022-08-04 01:10:00 +0430

    rename and creat a function to show Date

---
+++ 3-function/date.js
@@ -0,0 +1,26 @@
+function WhatsDay(Day){
+    switch(Day){
+        case 1:
+            document.write("<h1>،Today is Sat</h1>");
+            break;
+        case 2:
+            document.write('<h1>،Today is Sun</h1>');
+            break;
+        case 3:
+            document.write('<h1>،Today is Mon</h1>');
+            break;
+        case 4:
+            document.write('<h1>،Today is Thu</h1>');
+            break;
+        case 5:
+            document.write('<h1>،Today is Wen</h1>');
+            break;
+        case 6:
+            document.write('<h1>،Today is Thr</h1>');
+            break;
+        case 7:
+            document.write('<h1>،Today is Fri</h1>');
+            break;
+        }
+}
+WhatsDay(5);
