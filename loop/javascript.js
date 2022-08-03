987aad3565b5ad5fe3944a0a17a58450d3b85a12
emadsorush <emadsorush@gmail.com>
2022-08-03 22:54:01 +0430

    add style for table

--- loop/javascript.js
+++ loop/javascript.js
@@ -1,7 +1,7 @@
-document.write("<table 3px solid green>")
-for(let i=0; i<=10; i++){
+document.write("<table border='3px'>")
+for(let i=1; i<=10; i++){
     document.write('<tr>');
-    for(let j=0;j<=10;j++){
+    for(let j=1;j<=10;j++){
         document.write('<td>'+i*j+'</td>');
     }
     document.write('</tr>');
