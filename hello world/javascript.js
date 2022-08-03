888ea870555b6d3e6e957db7f770e1b21f14a4d2
emadsorush <emadsorush@gmail.com>
2022-08-03 22:27:05 +0430

    chenge javascript file

--- hello world/javascript.js
+++ hello world/javascript.js
@@ -1,9 +1,1 @@
-document.write("<table border:3px solid green>")
-for(let i=0; i<=10;i++){
-    document.write('<tr>');
-    for(let j=0;j<=10;j++){
-        document.write('<td>'+i*j+'</td>');
-    }
-    document.write('</td>');
-}
-document.write('</table>');
+document.write("<h1>hello world</h1>");
