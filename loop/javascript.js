517a02abb0e378a12b8b91a477d7df51198a2bce
emadsorush <emadsorush@gmail.com>
2022-08-03 22:46:16 +0430

    add a Multiplication table in javascript

--- loop/javascript.js
+++ loop/javascript.js
@@ -1,9 +1,9 @@
-document.write("<table border:3px solid green>")
-for(let i=0; i<=10;i++){
+document.write("<table 3px solid green>")
+for(let i=0; i<=10; i++){
     document.write('<tr>');
     for(let j=0;j<=10;j++){
         document.write('<td>'+i*j+'</td>');
     }
-    document.write('</td>');
+    document.write('</tr>');
 }
 document.write('</table>');
